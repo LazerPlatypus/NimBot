28e66c44d3e42d46336754669f2841b35c98ede1
tikalaka <baydadan001@gmail.com>
2020-01-22 14:24:34 -0700

    added two files to workk in

---
+++ aigame.js
@@ -0,0 +1,18 @@
+module.exports = {
+    name: 'takesticks',
+    description: 'Take a stick from a row in the game;',
+    guildOnly: false,
+    args: true,
+    cooldown: false,
+    execute(message, args) {
+        const Discord = require('discord.js');
+        var row = args[0];
+        var amount = args[1];
+        
+
+
+
+
+    }
+};
+
