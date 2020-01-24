ca1d5191ad183af62fc2ab9e03452a1072ddd78e
LazerPlatypus(Laptop) <aholt@student.neumont.edu>
2020-01-24 15:13:31 -0700

    buncha sht

--- commands/start.js
+++ commands/start.js
@@ -1,5 +1,6 @@
+const GameController = require('../helper_methods/GameController.js');
 module.exports = {
-    name: "Start",
+    name: "start",
     description: "Start the game of NIM.",
     aliases: ["begin"],
     guildOnly: false,
@@ -7,6 +8,20 @@ module.exports = {
     usage: "arguments are 'pve' or mention a player",
     cooldown: false,
     execute(message, args) {
-
+        let difficulty = 0
+        args.forEach((arg) => {
+            if (arg.length == 1) {
+                difficulty = parseInt(arg);
+            } 
+        });
+        let player1 = message.author;
+        if (message.mentions.user == null) {
+            GameController.createGame(player1, null, difficulty);
+        } else if (message.mentions.user.size != 1) {
+            // error here
+        } else {
+            console.log(message.mentions.user);
+            // GameController.createGame(player1, message.mentions.user.get(1), difficulty);
+        }
     },
 };
---
+++ commands/take.js
@@ -0,0 +1,17 @@
+module.exports = {
+    name: "take",
+    description: "take an amount of pipes from a specified pile",
+    aliases: 'grab',
+    guildOnly: false,
+    args: false,
+    usage: "take [pile to take from] [number of pipes to take]",
+    cooldown: false,
+    cooldowntime: null,
+    execute(message, args) {
+        // find users game
+            // call take
+                // display error if exists
+                // display success if exists
+        // error if not exists
+    },
+};
