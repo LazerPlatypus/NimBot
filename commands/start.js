3161038471d9b951593cb1675d2a3a441348c4bd
WhiteHatHacker1 <eonsahead@protonmail.com>
2020-01-24 14:31:57 -0700

    Filled out information for all commands.

--- commands/start.js
+++ commands/start.js
@@ -3,10 +3,11 @@ module.exports = {
     description: "Start the game of NIM.",
     aliases: ["begin"],
     guildOnly: false,
-    args: false,
-    usage: "Supply no arguments.",
-    cooldown: false,
+    args: true,
+    usage: "Enter the command followed by a difficulty level from 1 (easy) to 3 (hard) and a playmode of 1 (one-player) or 2 (two-player).",
+    cooldown: true,
+	cooldowntime: 10,
     execute(message, args) {
 		
-    },
+    }
 };
---
+++ commands/take.js
@@ -0,0 +1,13 @@
+module.exports = {
+    name: "Take",
+    description: "Take, or \"nim\", the specified number of pipes in the specified heap.",
+    aliases: ["nim"],
+    guildOnly: false,
+    args: true,
+    usage: "Enter the command followed by the number of pipes to take/nim and the heap from which to do so.",
+    cooldown: true,
+    cooldowntime: 5,
+    execute(message, args) {
+
+    }
+};
