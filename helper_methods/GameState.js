c776018b13a805d38532328d57d5ceb52e68affb
Searjasub Lopez <slopez@student.neumont.edu>
2020-01-24 14:52:55 -0700

    send-text.js

--- helper_methods/GameState.js
+++ helper_methods/GameState.js
@@ -7,7 +7,7 @@ class GameState  {
     player1 = null; // player 1 username
     player2 = null; // player 2 username or null if computer
     whosTurn = null; // 1 for player 1, 2 for player 2 or computer
-    difficulty = null; // 1, 2 or 3 for easy, medium, hard respectivly
+    difficulty = null; // 1, 2 or 3 for easy, medium, hard respectively
     pile1pipes = null; // number of pipes in pile 1
     pile2pipes = null; // numer of pipes in pile 2
     pile3pipes = null; // number of pipes in pile 3, or null if easy
---
+++ helper_methods/send-text.js
@@ -0,0 +1,26 @@
+module.exports = {
+    name: 'send-text',
+    description: 'testing a description',
+    author: '',
+    title: '',
+    text: '',
+
+    clearMessage() {
+        this.color = [0, 153, 255];
+        this.author = '';
+        this.title = '';
+        this.text = '';
+    },
+    setAuthor(authorText) {
+        this.author = authorText;
+    },
+    setTitle(title) {
+        this.title = title;
+    },
+    addText(text) {
+        this.text += text;
+    },
+    sendMessage(target){
+        const Discord = require('discord.js');
+    }
+};
