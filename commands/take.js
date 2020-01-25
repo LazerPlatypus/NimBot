3ab9def2ab65e83376424e5f44130a35060e2017
WhiteHatHacker1 <eonsahead@protonmail.com>
2020-01-24 19:19:23 -0700

    Fixed git commit error
    A new take.js file was uploaded, without a record of the old version being modified or even removed.  This restores the old version while retaining the alias and argument configuration of the newer version.

--- commands/take.js
+++ commands/take.js
@@ -1,17 +1,17 @@
 module.exports = {
     name: "take",
-    description: "take an amount of pipes from a specified pile",
-    aliases: 'grab',
+    description: "Take, or \"nim\", the specified number of pipes in the specified heap.",
+    aliases: ["nim", "grab"],
     guildOnly: false,
-    args: false,
-    usage: "take [pile to take from] [number of pipes to take]",
-    cooldown: false,
-    cooldowntime: null,
+    args: true,
+    usage: "Enter the command followed by the number of pipes to take/nim and the heap from which to do so.",
+    cooldown: true,
+    cooldowntime: 2,
     execute(message, args) {
         // find users game
             // call take
                 // display error if exists
                 // display success if exists
         // error if not exists
-    },
+    }
 };
