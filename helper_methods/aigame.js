279c2fe87b713988958be250a21eb7be3a44b8b0
tikalaka <baydadan001@gmail.com>
2020-01-27 12:44:28 -0700

    ai?

---
+++ helper_methods/aigame.js
@@ -0,0 +1,73 @@
+const gc = require('.\\GameController.js');
+module.exports = {
+    takeTurn(game){
+        if(game.difficulty == 1){
+            var randomRow = Math.floor(Math.random(2) * 10);
+            if(randomRow == 1 && game.amountofsticksinrow1 > 0){
+                var takeAmount = Math.floor(Math.random(game.pile1pipes) * 10);
+                game.pile1pipes -= takeAmount;
+            }
+            else if(randomRow == 2 && game.amountofsticksinrow2 > 0 ||
+                game.pile1pipes == 0 && game.amountofsticksinrow2 > 0){
+                var takeAmount = Math.floor(Math.random(game.pile2pipes) * 10);
+                game.pile2pipes -= takeAmount;
+            }
+        }
+        else if(game.difficulty == 2){
+            var randomRow = Math.floor(Math.random(3) * 10);
+            if(randomRow == 1 && game.pile1pipes > 0){
+                var takeAmount = Math.floor(Math.random(game.pile1pipes) * 10);
+                game.pile1pipes =- takeAmount;
+            }
+            else if(randomRow == 2 && game.pile2pipes > 0 ||
+                game.pile1pipes == 0 && game.pile2pipes > 0){
+                var takeAmount = Math.floor(Math.random(game.pile2pipes) * 10);
+                game.pile2pipes =- takeAmount;
+            }
+            else if(randomRow == 3 && game.pile3pipes > 0 ||
+                game.pile1pipes == 0 && game.pile3pipes > 0 &&
+                game.pile2pipes == 0 && game.pile3pipes > 0){
+                var takeAmount = Math.floor(Math.random(game.pile3pipes) * 10);
+                game.pile3pipes =- takeAmount;
+            }
+            
+        }
+        else if(game.difficulty == 3){
+            var randomRow = Math.floor(Math.random(3) * 10);
+            if(randomRow == 1 && game.amountofsticksinrow1 > 0){
+                var takeAmount = Math.floor(Math.random(game.pile1pipes) * 10);
+                game.pile1pipes =- takeAmount;
+            }
+            else if(randomRow == 2 && game.amountofsticksinrow2 > 0 ||
+                /*CHECK if the amount of sticks in the previous row is emtpy*/ amountofsticksinrow1 == 0 && game.amountofsticksinrow2 > 0){
+                var takeAmount = Math.floor(Math.random(/*amount of sticks left*/) * 10)
+                /*Then you would take the sticks from row 2 here*/
+            }
+            else if(randomRow == 3 && game.amountofsticksinrow2 > 0 ||
+                /*CHECK if the amount of sticks in the previous row is emtpy*/ amountofsticksinrow1 == 0 && game.amountofsticksinrow2 > 0 ||
+                /*CHECK if the amount of sticks in the previous row is emtpy*/ amountofsticksinrow2 == 0 && game.amountofsticksinrow3 > 0){
+                var takeAmount = Math.floor(Math.random(/*amount of sticks left*/) * 10)
+                /*Then you would take the sticks from row 3 here*/
+            }
+            else if(randomRow == 3 && game.amountofsticksinrow3 > 0 ||
+                game.amountofsticksinrow2 == 0 && game.amountofsticksinrow2 > 0 ||
+                game.amountofsticksinrow3 == 0 && game.amountofsticksinrow3 > 0 ||
+                game.amountofsticksinrow4 == 0 && game.amountofsticksinrow4 > 0){
+{
+                var takeAmount = Math.floor(Math.random(/*amount of sticks left*/) * 10)
+                /*Then you would take the sticks from row 3 here*/
+            }
+        }
+
+
+
+
+        //here we would do something that ould make a new game state but remove the 'amount of sticks from the selected row.
+        //but before we do that we also need to check how many sticks there are in the row to accoun for bad and incorrect input
+        //after we make a new gamestate we just need the AI (computer) to remove some sticks and make a new game state
+        //we can store game states in an array list in memory
+        //FINALLY, we will display the game state to the screen in the discord text chat with a ping(or not) and then the player can take their turn again
+        gamestate1 = st.gameState
+    }
+};
+
